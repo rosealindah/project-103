7f906275c03058dc7f4ebe2262b5c53644a363c4
rosealindah <76764225+rosealindah@users.noreply.github.com>
2021-06-22 10:26:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captureimage" src="'+data_uri+'">';
+    });
+}
+console.log("ml5 version",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/_SCX-Nsi3/model.json",modelloaded);
+function modelloaded(){
+    console.log("teachable machine has been loaded")
+}
